3e4a10600f034acc1bb45ede0ce4c8cbc4caf3fb
HÙNG ERM <114762186+hungvhutc281@users.noreply.github.com>
2023-10-04 17:00:26 +0700

    Add files via upload

---
+++ Controllers/LearnerController.cs
@@ -0,0 +1,57 @@
+﻿using lap4.Data;
+using lap4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace lap4.Controllers
+{
+    public class LearnerController : Controller
+    {
+        private SchoolContext db;
+        public LearnerController(SchoolContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var learners = db.Learners.Include(m => m.Major).ToList();
+            return View(learners);
+        }
+
+        public IActionResult Create()
+        {
+            //dùng 1 trong 2 cách để tạo SelectList gửi về View qua ViewBag để
+            //hiển thị danh sách chuyên ngành (Majors)
+            var majors = new List<SelectListItem>(); //cách 1
+            foreach (var item in db.Majors)
+            {
+                majors.Add(new SelectListItem
+                {
+                    Text = item.MajorName,
+
+                    Value = item.MajorID.ToString()
+                });
+
+            }
+            ViewBag.MajorID = majors;
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName"); //cách 2
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FirstMidName,LastName,MajorID,EnrollmentDate")]
+Learner learner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Learners.Add(learner);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            //lại dùng 1 trong 2 cách tạo SelectList gửi về View để hiển thị danh sách Majors
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+            return View();
+        }
+    }
+}
---
+++ Data/DbInitializer.cs
@@ -0,0 +1,70 @@
+﻿using lap4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lap4.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SchoolContext(serviceProvider
+            .GetRequiredService<DbContextOptions<SchoolContext>>()))
+            {
+                context.Database.EnsureCreated();
+                if (context.Learners.Any())
+                {
+                    return;
+                }
+                var majors = new Major[] {
+                new Major{MajorID = 1, MajorName="IT"},
+                new Major{MajorID = 2, MajorName="Economics"},
+                new Major{MajorID = 3, MajorName="Mathematics"},
+                };
+                foreach (var major in majors)
+                {
+                    var existingMajor = context.Majors.Find(major.MajorID);
+                    if (existingMajor == null)
+                    {
+                        context.Majors.Add(major);
+                    }
+                }
+               
+                context.SaveChanges();
+                var learners = new
+                Learner[] {
+                    new Learner {LearnerID=1, FirstMidName = "Carson", LastName = "Alexander",
+                    EnrollmentDate = DateTime.Parse("2005-09-01") , MajorID = 1},
+                    new Learner {LearnerID=2, FirstMidName = "Meredith", LastName = "Alonso",
+                    EnrollmentDate = DateTime.Parse("2002-09-01"), MajorID = 2 }
+                };
+                foreach (Learner l in learners)
+                {
+                    context.Learners.Add(l);
+                }
+                context.SaveChanges();
+                var courses = new Course[]{
+                    new Course{CourseID=1050,Title="Chemistry",Credits=3},
+                    new Course{CourseID=4022,Title="Microeconomics",Credits=3},
+                    new Course{CourseID=4041,Title="Macroeconomics",Credits=3}
+                };
+                foreach (Course c in courses)
+                {
+                    context.Courses.Add(c);
+                }
+                context.SaveChanges();
+                var enrollments = new Enrollment[]{
+                    new Enrollment{EnrollmentID = 1, LearnerID=1,CourseID=1050,Grade=5.5f},
+                    new Enrollment{EnrollmentID = 2, LearnerID=1,CourseID=4022,Grade=7.5f},
+                    new Enrollment{EnrollmentID = 1, LearnerID=2,CourseID=1050,Grade=3.5f},
+                    new Enrollment{EnrollmentID = 3, LearnerID=2,CourseID=4041,Grade=7f}
+
+                };
+                foreach (Enrollment e in enrollments)
+                {
+                    context.Enrollments.Add(e);
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
